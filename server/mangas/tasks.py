c7a649033f22260df4640b350d17b61223eafa23
Farid Ansari <farid12ansari7@gmail.com>
2020-04-07 01:35:35 +0530

    fixed linting

--- server/mangas/tasks.py
+++ server/mangas/tasks.py
@@ -3,7 +3,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from mangas import updater
 
+
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(updater.update_manga_data, 'interval', minutes=1)
+    scheduler.add_job(updater.update_manga_data, "interval", minutes=1)
     scheduler.start()
--- server/mangas/updater.py
+++ server/mangas/updater.py
@@ -2,9 +2,10 @@ import requests
 from mangas.models import Mangas
 from mangas.serializers import MangasSerializer
 
+
 def get_manga_data():
-    url = 'https://www.mangaeden.com/api/list/0/'
-  
+    url = "https://www.mangaeden.com/api/list/0/"
+
     r = requests.get(url)
 
     try:
@@ -13,32 +14,36 @@ def get_manga_data():
     except:
         return None
 
+
 def update_manga_data():
     json = get_manga_data()
     if json is not None:
         data = []
         for x in json["manga"]:
             if "ld" in x and "ld" is not None:
-                data.append({
-                    "alias": x["a"],
-                    "categories": x["c"],
-                    "id": x["i"],
-                    "image": x["im"],
-                    "status": x["s"],
-                    "title": x["t"],
-                    "hits": x["h"],
-                    "last_updated": x["ld"]
-                })
+                data.append(
+                    {
+                        "alias": x["a"],
+                        "categories": x["c"],
+                        "id": x["i"],
+                        "image": x["im"],
+                        "status": x["s"],
+                        "title": x["t"],
+                        "hits": x["h"],
+                        "last_updated": x["ld"],
+                    }
+                )
         print(data)
         MangasSerializer(many=True).create(data)
-      
+
+
 # def update_forecast():
 #     json = _get_forecast_json()
 #     if json is not None:
 #         try:
 #             new_forecast = Forecast()
-            
-#             # open weather map gives temps in Kelvin. We want celsius.              
+
+#             # open weather map gives temps in Kelvin. We want celsius.
 #             temp_in_celsius = json['main']['temp'] - 273.15
 #             new_forecast.temperatue = temp_in_celsius
 #             new_forecast.description = json['weather'][0]['description']
