8629ccd399b74a54b5857022ce1ba48a6b5060c4
Farid Ansari <farid12ansari7@gmail.com>
2020-04-10 04:20:09 +0530

    fixed ordering

--- server/mangas/filters.py
+++ server/mangas/filters.py
@@ -1,6 +1,7 @@
 from django_filters import rest_framework as filters
 from mangas.models import Mangas
 
+
 class MangasFilter(filters.FilterSet):
     class Meta:
         model = Mangas
--- server/mangas/models.py
+++ server/mangas/models.py
@@ -3,15 +3,16 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
+
 class Mangas(models.Model):
     id = models.CharField(primary_key=True, max_length=24)
     alias = models.TextField(max_length=30)
     categories = ArrayField(models.CharField(max_length=200), blank=True, default=list)
-    hits= models.IntegerField(null=True)
+    hits = models.IntegerField(null=True)
     status = models.IntegerField(null=True)
     title = models.TextField(null=True)
     last_updated = models.IntegerField(null=True)
     image = models.TextField(null=True)
 
     class Meta:
-        ordering: ["-last_updated"]
+        ordering = ["-hits"]
