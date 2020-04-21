78223c8c522c46fe8207b6c761be69f35a2bd9bc
Farid Ansari <farid12ansari7@gmail.com>
2020-04-21 21:56:17 +0530

    added paths

--- client/src/config/import_paths.js
+++ client/src/config/import_paths.js
@@ -1,10 +1,12 @@
 export const Screens = {
   Home: () => require('../screens/home/home').default,
   Manga: () => require('../screens/manga').default,
+  MangaChapter: () => require('../screens/manga_chapter/manga_chapter').default,
 };
 
 export const Libs = {
   Networking: () => require('../libs/networking'),
+  Utils: () => require('../libs/utils'),
 };
 
 
@@ -12,4 +14,12 @@ export const CustomHooks = {
   useLocalStorage: () => require('../hooks/useLocalStorage'),
   useDataApi: () => require('../hooks/useDataApi'),
   useFavourtiedManga: () => require('../hooks/useFavouritedManga'),
+};
+
+export const Urls = {
+  ApiUrls: () => require('./api_urls'),
+};
+
+export const Components = {
+  LazyLoadImage: () => require('../components/lazy_load_image/lazy_load_image'),
 };
