f2bddb0571ba229edfd8913785a64c0f8c5f3ebf
Farid Ansari <farid12ansari7@gmail.com>
2020-08-06 02:42:44 +0530

    added manga chapter page

--- client/src/screens/manga_chapter/manga_chapter.jsx
+++ client/src/screens/manga_chapter/manga_chapter.jsx
@@ -1,47 +1,130 @@
-import React, { useRef } from 'react';
-import { shape, string } from 'prop-types';
-import { Spin } from 'antd';
+import React, { useEffect, useRef } from 'react';
+import { func, shape, string } from 'prop-types';
+import { Button, PageHeader, Select, Spin, Typography } from 'antd';
+import Headroom from 'react-headroom';
+import _ from 'lodash';
+
 import './manga_chapter.less';
 
-import { Components, CustomHooks, Urls } from '#config/import_paths';
+import { Components, CustomHooks, Libs, Urls } from '#config/import_paths';
+import { NextPageBox } from './next_page_box';
+
+const { Option } = Select;
+
+const { SanitiazeTitle } = Libs.Utils();
 
 const { LazyLoadImage } = Components.LazyLoadImage();
-
-const { useDataApi } = CustomHooks.useDataApi();
+const { useDataApi } = CustomHooks.UseDataApi();
 
 const { ApiUrls } = Urls.ApiUrls();
 
-
-const MangaChapter = ({ match }) => {
+let isDropdownVisible = false;
+const MangaChapter = ({ match, history }) => {
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
-    ApiUrls.baseUrl + ApiUrls.mangaChapter.replace('{chapterId}', match.params.chapterId),
-    { results: [] },
+    ApiUrls.baseUrl
+    + ApiUrls.mangaChapter.replace('{chapterId}', `${match.params.mangaId}-${match.params.chapterId}`),
+    null,
     false,
   );
-  const wrapperRef = useRef();
+  const chapterMenuRef = useRef(null);
 
-  if (isLoading || !data) {
-    return (
-      <div className = "mangaChapterSpinner"><Spin size = "large" /></div>
-    );
-  }
+  useEffect(() => {
+    window.addEventListener('scroll', _onScroll);
+    doFetch(ApiUrls.baseUrl
+      + ApiUrls.mangaChapter.replace('{chapterId}', `${match.params.mangaId}-${match.params.chapterId}`));
+
+    return () => {
+      window.addEventListener('scroll', _onScroll);
+    };
+  }, [match.params.chapterId]);
+
+  const _returnChapterPath = (mangaId, chapterId, mangaTitle) => `${mangaId}-${chapterId}-${SanitiazeTitle(mangaTitle)}`;
+
+  const _navigateToChapter = (chapterId) => {
+    const { mangaId, mangaName } = match.params;
+    const path = _returnChapterPath(mangaId, chapterId, mangaName);
+    history.replace(path);
+  };
+
+  const _onSelectChapter = (chapterNumber) => {
+    _navigateToChapter(chapterNumber);
+  };
+
+  const _onScroll = () => {
+    if (isDropdownVisible) chapterMenuRef.current.blur();
+  };
+
+  const _onDropdownVisibleChange = (isVisible) => {
+    isDropdownVisible = isVisible;
+  };
+
+  const _onPrevChapter = () => {
+    _navigateToChapter(data.prev_chapter_id);
+  };
+
+  const _onNextChapter = () => {
+    _navigateToChapter(data.next_chapter_id);
+  };
 
   return (
-    <div ref = {wrapperRef} className = "mangaChapterWrapper">
+    <div className = "mangaChapterWrapper" style = {isLoading ? { height: '100vh' } : {}}>
       {
-        [...data]
-          .reverse()
-          .map((item) => (
+        !data ? null : (
+          <div style = {{ width: '100%' }}>
+            <Headroom id = "area">
+              <PageHeader
+                className = "mangaChaperPageHeader"
+                onBack = {() => null}
+              >
+                <div className = "mangaChaperPageHeaderContent">
+                  <Typography.Title style = {{ color: 'white' }} level = {4}>
+                    {data.manga_title}
+                  </Typography.Title>
+                  <div style = {{ flex: 1 }} />
+                  <Button ghost onClick = {_onPrevChapter}>Prev Chapter</Button>
+                  <Button ghost style = {{ marginLeft: 7 }} onClick = {_onNextChapter}>Next Chapter</Button>
+                </div>
+                <Select
+                  ref = {chapterMenuRef}
+                  getPopupContainer = {() => document.getElementById('area')}
+                  onDropdownVisibleChange = {_onDropdownVisibleChange}
+                  className = "mangaChapterList"
+                  dropdownClassName = "mangaChapterListMenu"
+                  value = {match.params.chapterId}
+                  onChange = {_onSelectChapter}
+                >
+                  {
+                    data.chapters.map((chapter) => (
+                      <Option
+                        key = {chapter.number}
+                        className = "mangaChapterChaptersOption"
+                        value = {chapter.number}
+                      >
+                        {chapter.title}
+                      </Option>
+                    ))
+                  }
+                </Select>
+              </PageHeader>
+            </Headroom>
+          </div>
+        )
+      }
+      {isLoading || !data ? (
+        <div className = "mangaChapterSpinner">
+          <Spin style = {{ zoom: 1.5 }} size = "large" />
+        </div>
+
+      ) : (
+        <>
+          {data.images.map((item) => (
             <div key = {item.id}>
-              <LazyLoadImage
-                placeholderHeight = {item.image.height}
-                rootRef = {wrapperRef}
-                referrerPolicy = "no-referrer"
-                src = {`https://cdn.mangaeden.com/mangasimg/${item.image.url}`}
-              />
+              <LazyLoadImage src = {item.url} />
             </div>
-          ))
-  }
+          ))}
+          <NextPageBox chapterId = {data.next_chapter_id} navigateToChapter = {_navigateToChapter} />
+        </>
+      )}
     </div>
   );
 };
@@ -51,6 +134,9 @@ MangaChapter.propTypes = {
     params: shape({
       chapterId: string.isRequired,
     }).isRequired,
+  }).isRequired,
+  history: shape({
+    replace: func.isRequired,
   }).isRequired,
 };
 
