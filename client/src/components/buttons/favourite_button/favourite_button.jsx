bbe80b2a96693e731df043a29294e3152148e411
Farid Ansari <farid12ansari7@gmail.com>
2020-08-10 00:48:48 +0530

    moved favourite_button as a shared component

---
+++ client/src/components/buttons/favourite_button/favourite_button.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { StarFilled, StarOutlined } from '@ant-design/icons';
+import { bool, func, shape, string } from 'prop-types';
+
+import './favourite_button.less';
+
+const FavouriteButton = React.forwardRef(({ item, isFavourite, onClick }, ref) => {
+  const [iconClassName, setIconClassName] = useState('');
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (didMountRef.current) {
+      if (isFavourite) setIconClassName('favourited');
+      else setIconClassName('');
+    } else didMountRef.current = true;
+  }, [isFavourite]);
+
+  const _onClick = (e) => {
+    e.stopPropagation();
+    onClick(item);
+  };
+
+  const IconComponent = isFavourite
+    ? StarFilled : StarOutlined;
+
+  return (
+    <div className = "favouriteButtonWrapper">
+      <IconComponent
+        ref = {ref}
+        style = {{ fontSize: 16, padding: 3, color: isFavourite ? '#FFD700' : 'inherit' }}
+        onClick = {_onClick}
+        className = {iconClassName}
+      />
+    </div>
+  );
+});
+
+FavouriteButton.propTypes = {
+  item: shape({
+    id: string.isRequired,
+  }).isRequired,
+  isFavourite: bool.isRequired,
+  onClick: func.isRequired,
+};
+
+export default FavouriteButton;
